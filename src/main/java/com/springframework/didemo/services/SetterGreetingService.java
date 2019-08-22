76cf20665bed5a36705c8d1313815b5d99842701
Andres Andrade <a.andrade09@yahoo.com>
2019-08-22 13:30:12 -0700

    DI services for constructor, getter and greeting service

--- src/main/java/com/springframework/didemo/services/SetterGreetingService.java
+++ src/main/java/com/springframework/didemo/services/SetterGreetingService.java
@@ -3,7 +3,7 @@ package com.springframework.didemo.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class GetterGreetingService implements GreetingService {
+public class SetterGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
