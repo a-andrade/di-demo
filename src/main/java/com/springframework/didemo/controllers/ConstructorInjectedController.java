6a58c504e857640c371e9537a99efc917f750539
Andres Andrade <a.andrade09@yahoo.com>
2019-08-22 13:14:49 -0700

    DI controllers for constructor, property and setter

---
+++ src/main/java/com/springframework/didemo/controllers/ConstructorInjectedController.java
@@ -0,0 +1,20 @@
+package com.springframework.didemo.controllers;
+
+import com.springframework.didemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ConstructorInjectedController {
+
+    private GreetingService greetingService;
+
+    @Autowired // constructor DI automatically autowired (@Autowired not required) [optional]
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
+    }
+}
--- src/main/java/com/springframework/didemo/controllers/MyController.java
+++ src/main/java/com/springframework/didemo/controllers/MyController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MyController {
 
-
-
+    public String hello() {
+        System.out.println("Hello");
+        return null;
+    }
 }
