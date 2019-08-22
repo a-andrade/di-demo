7e9fc9fb380d0a9cd16ee04d1a8dd5c4253cc7b9
Andres Andrade <a.andrade09@yahoo.com>
2019-08-22 13:32:05 -0700

    Added @qualifier annotation

--- src/main/java/com/springframework/didemo/controllers/SetterInjectedController.java
+++ src/main/java/com/springframework/didemo/controllers/SetterInjectedController.java
@@ -2,6 +2,7 @@ package com.springframework.didemo.controllers;
 
 import com.springframework.didemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -14,6 +15,7 @@ public class SetterInjectedController {
     }
 
     @Autowired
+    @Qualifier("setterGreetingService")
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
