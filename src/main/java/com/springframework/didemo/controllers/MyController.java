e046ba165e667fb93d160fcb580ac2d64f1b4964
Andres Andrade <a.andrade09@yahoo.com>
2019-08-22 13:52:13 -0700

    Updated output message

--- src/main/java/com/springframework/didemo/controllers/MyController.java
+++ src/main/java/com/springframework/didemo/controllers/MyController.java
@@ -1,12 +1,19 @@
 package com.springframework.didemo.controllers;
 
+import com.springframework.didemo.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String hello() {
-        System.out.println("Hello");
-        return null;
+        System.out.println("Hello - from the myController");
+        return greetingService.sayGreeting();
     }
 }
