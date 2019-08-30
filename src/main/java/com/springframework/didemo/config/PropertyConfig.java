53f6f65326bdfe46da5828ada163cd40720ed97a
Andres Andrade <a.andrade09@yahoo.com>
2019-08-30 16:44:44 -0700

    Added environment variable for practice.

--- src/main/java/com/springframework/didemo/config/PropertyConfig.java
+++ src/main/java/com/springframework/didemo/config/PropertyConfig.java
@@ -1,15 +1,22 @@
 package com.springframework.didemo.config;
 
 import com.springframework.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    // environment variable override
+    //useful in cases like not wanting to store passwords in a file
+    @Autowired
+    Environment environment;
 
     @Value("${com.username}")
     private String username;
@@ -23,7 +30,8 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(username);
+        //fakeDataSource.setUser(username);
+        fakeDataSource.setUser(environment.getProperty("USERNAME")); // environment variable override
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(dburl);
         return fakeDataSource;
